d9a7ae0e647a4f1f2be679b19df7c9eb395c8882
Joby Bejoy <jobybejoy@gmail.com>
2021-03-12 20:58:07 -0500

    Helper Label Component

--- src/components/Form/Label/index.js
+++ src/components/Form/Label/index.js
@@ -5,3 +5,9 @@ export default function Label({ htmlFor, children, className }) {
     <label htmlFor={htmlFor} className={styles.input_label + " " + styles[className]}>{children}</label>
   )
 }
+
+export function Helper({ htmlFor, children, className }) {
+  return (
+    <label htmlFor={htmlFor} className={`${styles.input_label} ${styles.helper} ${styles[className]}`}>{children}</label>
+  )
+}

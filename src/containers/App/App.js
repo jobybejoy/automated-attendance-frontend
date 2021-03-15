f539810b54d67b61c9b62ccd77838c62d1896c5a
Joby Bejoy <57924079+jobybejoy@users.noreply.github.com>
2021-03-15 15:05:07 -0400

    Provider - User and Sessions added to App.js

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react"
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
-import useToken from "./useToken";
 
 // Public Routes
 import Login from "../../pages/Auth/Login"
@@ -14,6 +13,7 @@ import Profile from "../../pages/Profile"
 import EditProfile from "../../pages/Profile/Edit"
 import StudentAttendance from "../../pages/Attendance/Student/index"
 
+import useToken from "../../api/auth/useToken";
 import useUser from "../../api/user"
 import useSessions from "../../api/sessions"
 
@@ -29,14 +29,27 @@ const RequireAuth = ({ children, token }) => {
   return children;
 };
 
+const ProviderWrapper = ({ children, token }) => {
+
+  const { user, isError, isLoading } = useUser({ token });
+  const { sessions, isError: isSessionsError, isLoading: isSessionsLoading } = useSessions({ token });
+  return (
+    <UserContext.Provider value={{ user, isError, isLoading }}>
+      <SessionsContext.Provider value={{ sessions, isError: isSessionsError, isLoading: isSessionsLoading }}>
+        {children}
+      </SessionsContext.Provider>
+    </UserContext.Provider>
+  )
+}
+
 function App() {
 
-  //Using custom Token Hook 
+  //Using custom Token Hook
   const { token, setToken } = useToken();
-  const { user, isError, isLoading } = useUser();
-  const { sessions, isError: isSessionsError, isLoading: isSessionsLoading } = useSessions();
-  // console.log({ sessions });
-  // const [user, setUser] = useState({ name: "John doe", email: "email@njit.edu", department: "Computer Science", phone_number: "+1 551 263 9920", address: "Some place, Sowhere\nState country\nzip" })
+  // const { user, isError, isLoading } = useUser();
+  // const { sessions, isError: isSessionsError, isLoading: isSessionsLoading } = useSessions();
+  // console.log({sessions});
+  // const [user, setUser] = useState({name: "John doe", email: "email@njit.edu", department: "Computer Science", phone_number: "+1 551 263 9920", address: "Some place, Sowhere\nState country\nzip" })
 
   return (
     <div className="App">
@@ -48,20 +61,20 @@ function App() {
           <Route exact path="/auth/password/reset" component={ResetPassword}></Route>
 
           <RequireAuth token={token}>
-            <UserContext.Provider value={{ user, isError, isLoading }}>
-              <SessionsContext.Provider value={{ sessions, isError: isSessionsError, isLoading: isSessionsLoading }}>
-                <Route path="/" exact component={() => <Home />} />
-                <Route path="/:department/:course_number/session/:session_id" component={() => <StudentAttendance />} />
-              </SessionsContext.Provider>
+            <ProviderWrapper token={token}>
+
+              <Route path="/" exact component={() => <Home />} />
+              <Route path="/:department/:course_number/session/:session_id" component={() => <StudentAttendance />} />
 
               <Route path="/profile" exact component={() => <Profile />} />
               <Route path="/profile/edit" exact component={() => <EditProfile />} />
-            </UserContext.Provider>
+
+            </ProviderWrapper>
           </RequireAuth>
 
         </Switch>
-      </BrowserRouter>
-    </div>
+      </BrowserRouter >
+    </div >
   );
 }
 

b5a294fdd6fe0fa63408f227e733be29b0ce65fe
Joby Bejoy <57924079+jobybejoy@users.noreply.github.com>
2021-03-15 16:07:50 -0400

    Component | Session (linked to API)

--- src/components/Session/SessionList/SessionItem.js
+++ src/components/Session/SessionList/SessionItem.js
@@ -6,11 +6,11 @@ export default function SessionItem({ session, key }) {
     <Link to={session.url} key={key}>
       <div className={styles.session_card}>
         <div>
-          <div className={styles.course_id}>{session.course_id}</div>
-          <div className={styles.session_id}>{session.session_id}</div>
+          <div className={styles.course_id}>{session.course_string}</div>
+          <div className={styles.session_id}>{session.session_name}</div>
         </div>
         <div className={styles.course_name}>{session.course_name}</div>
-        <div className={styles.term}>{session.term}</div>
+        <div className={styles.term}>{session.term + " " + session.year}</div>
       </div>
     </Link>
   )

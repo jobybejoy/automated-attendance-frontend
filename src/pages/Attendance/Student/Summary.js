e1d4b6bf91303153c2d1da89a0184fc4cb0db737
Joby Bejoy <57924079+jobybejoy@users.noreply.github.com>
2021-03-15 15:06:37 -0400

    Pages | Modified Student Attendance Page - Summary

---
+++ src/pages/Attendance/Student/Summary.js
@@ -0,0 +1,26 @@
+import styles from "./StudentAttendance.module.css"
+
+const NumberInMinTwoDigits = (number) => {
+  if (!number) return "--"
+  return (number < 10) ? '0' + number.toString() : number.toString();
+}
+
+function Sessions({ session_attendance }) {
+  return (
+    <div className={styles.summary_container}>
+      <div className={styles.summary_section_title}>Summary</div>
+      <div className={styles.summary_card}>
+        <h2 className={styles.summary_title_very_large}>{session_attendance?.percentage || "--%"}</h2>
+        <div className={styles.summary_subheading}>Attendance</div>
+      </div>
+      <div className={styles.summary_card}>
+        <h2 className={styles.summary_title_large}>{NumberInMinTwoDigits(session_attendance?.attended) || "-- "} / {NumberInMinTwoDigits(session_attendance?.all_sessions) || "--"}</h2>
+        <div className={styles.summary_subheading}>
+          <span>Attended</span>
+          <span style={{ marginLeft: session_attendance?.all_sessions ? "4rem" : "2rem" }}>All Sessions</span></div>
+      </div>
+    </div>
+  );
+}
+
+export default Sessions;
--- src/pages/Attendance/Student/index.js
+++ src/pages/Attendance/Student/index.js
@@ -2,42 +2,31 @@ import Layout from "../../../containers/Layout"
 import styles from "./StudentAttendance.module.css"
 
 import Calender from "../../../components/Calender"
+import Summary from "./Summary"
 import { CalenderItem, PercentageCalenderItem } from "../../../components/Calender/DateElement"
 
-export default function StudentAttendance({ params }) {
+import useAttendance from "../../../api/attendance"
 
-  const student_attendance = {
-    attendance_data: [
-      { date: "2/21/2021", type: "attended" },
-      { date: "2/22/2021", type: "attended" },
-      { date: "3/1/2021", type: "attended" },
-      { date: "3/2/2021", type: "attended" },
-      { date: "3/7/2021", type: "attended" },
-      { date: "3/9/2021", type: "absent" }
-    ],
-    attended: 24,
-    all_sessions: 28,
-    percentage: "79%",
+export default function StudentAttendanceWrapper() {
+
+  const { session_attendance, isError, isLoading } = useAttendance()
+
+  if (isLoading) {
+    return "Fetching Attendance Details"
   }
 
+
+  return <StudentAttendance session_attendance={session_attendance} />;
+
+}
+
+function StudentAttendance({ session_attendance }) {
 
   return (
     <Layout>
       <div className={styles.attendance_container}>
-        <Calender className={styles.attendance_calender} data={student_attendance.attendance_data} DateComponent={CalenderItem} />
-        <div className={styles.summary_container}>
-          <div className={styles.summary_section_title}>Summary</div>
-          <div className={styles.summary_card}>
-            <h2 className={styles.summary_title_very_large}>{student_attendance?.percentage || "--%"}</h2>
-            <div className={styles.summary_subheading}>Attendance</div>
-          </div>
-          <div className={styles.summary_card}>
-            <h2 className={styles.summary_title_large}>{student_attendance.attended || "-- "} / {student_attendance.all_sessions || "--"}</h2>
-            <div className={styles.summary_subheading}>
-              <span>Attended</span>
-              <span style={{ marginLeft: student_attendance.all_sessions ? "4rem" : "2rem" }}>All Sessions</span></div>
-          </div>
-        </div>
+        <Calender className={styles.attendance_calender} data={session_attendance?.attendance_data} DateComponent={CalenderItem} />
+        <Summary session_attendance={session_attendance} />
       </div>
     </Layout>
   )

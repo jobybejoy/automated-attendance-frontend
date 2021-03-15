599ea24bdff3b709baaa1d9cc72315e457262d02
Joby Bejoy <57924079+jobybejoy@users.noreply.github.com>
2021-03-15 16:22:33 -0400

    Navbar | getSession function

--- src/containers/Layout/Navbar/index.js
+++ src/containers/Layout/Navbar/index.js
@@ -9,20 +9,24 @@ import { useContext } from "react";
 import { UserContext } from "../../../context/UserContext"
 import { SessionsContext } from "../../../context/SessionsContext"
 
-const Header = ({ department, course_number, session_id, session, user }) => {
+const Header = ({ department, course_number, session_id, sessions, user }) => {
 
   if (department && course_number && session_id) {
+    const session = getSession({ sessions, department, course_number, session_id })
     return <SessionHeader session={session} />
   } else {
-    return <h1 className={styles.large_title}>Hello, {user?.name}</h1>
+    return <h1 className={styles.large_title}>Hello, {user?.first_name + " " + user?.last_name}</h1>
   }
 
 }
 
-const getSession = ({ sessions, course_number, session_id }) => {
+const getSession = ({ sessions, department, course_number, session_id }) => {
   // ! currently finding session only with sessionID
   // * Verfy the structure of session object
-  return sessions.find(x => (x.session_id === session_id));
+
+  console.log({ sessions, session_id, course_number })
+
+  return sessions.find(x => (x.session_name === session_id) && (x.course_string.toLowerCase() === (department + " " + course_number)));
   // && (x.course_id === course_number)
   // }
 }
@@ -31,8 +35,10 @@ function Navbar() {
 
   const { user } = useContext(UserContext)
   const { sessions, isError, isLoading } = useContext(SessionsContext)
+
   // Gets the department,course_id and  params from url
   const { department, course_number, session_id } = useParams();
+
   // const sessions = [
   //   { course_id: "CS 123", session_id: "111", course_name: "Data Structures and Algorithms", term: "Fall 2020", url: "/cs/123/session/111" },
   // { course_id: "CS 656", session_id: "001", course_name: "Machine Learning", term: "Fall 2020", url: "/cs/656/session/001" },
@@ -43,13 +49,10 @@ function Navbar() {
     return "Loading"
   }
 
-
-  const session = getSession({ sessions, course_number, session_id })
-
   return (
     <nav className={styles.nav_container}>
       {/* <h1 className={styles.large_title}>Hello, John Doe</h1> */}
-      <Header department={department} course_number={course_number} session_id={session_id} user={user} session={session} />
+      <Header department={department} course_number={course_number} session_id={session_id} user={user} sessions={sessions} />
 
       <Link to="/profile">
         <div className={styles.user_profile_container}>

21893f04797e9f2461236f223d2a6c9e48b5ae24
Joby Bejoy <57924079+jobybejoy@users.noreply.github.com>
2021-03-18 03:06:48 -0400

    Session | prop data and capitalized text

--- src/components/Session/Header/index.js
+++ src/components/Session/Header/index.js
@@ -6,9 +6,9 @@ export default function SessionHeader({ session }) {
   return (
     <div className={styles.session_header_container}>
       <div className={styles.session_meta_details}>
-        <span><b>{session.course_string.toUpperCase()}</b> </span>
-        <span>{session.session_name}</span>
-        <span>{session.term + " " + session.year}</span>
+        <span><b>{session?.course_string.toUpperCase()}</b> </span>
+        <span>{session?.session_name}</span>
+        <span>{session?.term + " " + session?.year}</span>
       </div>
       <h1 className={styles.course_name}>{session.course_name}</h1>
     </div >

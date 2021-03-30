f762f4cc6248559f9497e1ba7c86f3ec085201ce
Joby Bejoy <57924079+jobybejoy@users.noreply.github.com>
2021-03-29 22:01:04 -0400

    Fix - increased the interval to fetch session att.

--- src/api/attendance/Faculty/SessionAttendance.js
+++ src/api/attendance/Faculty/SessionAttendance.js
@@ -14,7 +14,7 @@ export default function useAllStudentAttendance(course_id, session_id, date) {
 
   console.log("Session Attendance Rquest fetch invoked");
   const { token } = useToken();
-  const { data, error } = useSWR([GET_SESSION_ATTENDANCE_ON_DATE_URL(course_id, session_id, date), token], fetcher)
+  const { data, error } = useSWR([GET_SESSION_ATTENDANCE_ON_DATE_URL(course_id, session_id, date), token], fetcher, { refreshInterval: 1000 })
 
   return {
     student_attendance: data,

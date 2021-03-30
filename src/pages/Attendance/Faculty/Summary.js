519782989953b3b6f22a22128841de08d80241aa
Joby Bejoy <57924079+jobybejoy@users.noreply.github.com>
2021-03-29 22:00:02 -0400

    Start Marking Attendance - API and button

--- src/pages/Attendance/Faculty/Summary.js
+++ src/pages/Attendance/Faculty/Summary.js
@@ -1,11 +1,12 @@
 import styles from "../Student/StudentAttendance.module.css"
+import { CTA_Button } from "../../../components/Button"
 
 const NumberInMinTwoDigits = (number) => {
   if (!number) return "--"
   return (number < 10) ? '0' + number.toString() : number.toString();
 }
 
-function Sessions({ session_attendance }) {
+function Sessions({ session_attendance, start_session_handler }) {
   return (
     <div className={styles.summary_container}>
       <div className={styles.summary_section_title}>Summary</div>
@@ -13,6 +14,7 @@ function Sessions({ session_attendance }) {
         <h2 className={styles.summary_title_very_large}>{session_attendance?.percentage || "--%"}</h2>
         <div className={styles.summary_subheading}>Avg. Attendance</div>
       </div>
+      <CTA_Button className={styles.cta_start_session} value="Start Session" onClick={start_session_handler} />
       {/* <div className={styles.summary_card}>
         <h2 className={styles.summary_title_large}>{NumberInMinTwoDigits(session_attendance?.attended) || "-- "} / {NumberInMinTwoDigits(session_attendance?.all_sessions) || "--"}</h2>
         <div className={styles.summary_subheading}>
--- src/pages/Attendance/Faculty/index.js
+++ src/pages/Attendance/Faculty/index.js
@@ -14,15 +14,27 @@ import useSessionAttendance from "../../../api/attendance/faculty"
 
 import { findSession } from "../helpers.js"
 
+import StartSession from "../../../api/attendance/Faculty/StartSession"
+
+import useToken from "../../../api/auth/useToken"
+import { useHistory, useLocation } from "react-router-dom"
+
 
 export default function FacultyAttendanceWrapper() {
 
   const { department, course_number, session_id } = useParams();
   const { sessions } = useContext(SessionsContext)
 
+
+  const today = new Date()
+
+  const { token } = useToken()
+  const history = useHistory();
+  const { pathname } = useLocation()
+
   const session = findSession(sessions, department, course_number, session_id);
 
-  console.log({ session });
+  // console.log({ session });
 
   const { session_attendance, isError, isLoading } = useSessionAttendance(session?.course_id, session?.session_id);
 
@@ -42,17 +54,40 @@ export default function FacultyAttendanceWrapper() {
     "percentage": getAveragePercentage(session_attendance)
   }
 
-  return <FacultyAttendance session_attendance={data} />;
+  const getDate = (today) => {
+    return {
+      date: today.getDate(),
+      month: today.getMonth() + 1,
+      year: today.getFullYear()
+    }
+  }
+
+  const getStartSessionURL = (pathname, today) => {
+    const { date, month, year } = getDate(today)
+    return (`${pathname}/attendance/on/${year}/${month}/${date}`)
+  }
+
+  function start_session_handler(token, course_id, session_id, today) {
+    StartSession(token, course_id, session_id, today)
+      .then(() => {
+        const url = getStartSessionURL(pathname, today)
+        history.push(url)
+      })
+      .catch(error => console.error(error))
+  }
+
+
+  return <FacultyAttendance session_attendance={data} start_session_handler={() => start_session_handler(token, session?.course_id, session?.session_id, today)} />;
 
 }
 
-function FacultyAttendance({ session_attendance }) {
+function FacultyAttendance({ session_attendance, start_session_handler }) {
 
   return (
     <Layout>
       <div className={styles.attendance_container}>
         <Calender className={styles.attendance_calender} data={session_attendance?.attendance_data || []} DateComponent={PercentageCalenderItem} />
-        <Summary session_attendance={session_attendance} />
+        <Summary session_attendance={session_attendance} start_session_handler={() => start_session_handler()} />
       </div>
     </Layout>
   )

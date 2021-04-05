934600900ca444da7acb46db5ad68cdd96d2b100
Joby Bejoy <57924079+jobybejoy@users.noreply.github.com>
2021-04-05 00:13:02 -0400

    NavBar w/ loading state

--- src/containers/Layout/Navbar/index.js
+++ src/containers/Layout/Navbar/index.js
@@ -9,13 +9,15 @@ import { useContext } from "react";
 import { UserContext } from "../../../context/UserContext"
 import { SessionsContext } from "../../../context/SessionsContext"
 
+import Skeleton from 'react-loading-skeleton';
+
 const Header = ({ department, course_number, session_id, sessions, user }) => {
 
   if (department && course_number && session_id) {
     const session = getSession({ sessions, department, course_number, session_id })
     return <SessionHeader session={session} />
   } else {
-    return <h1 className={styles.large_title}>Hello, {user?.first_name + " " + user?.last_name}</h1>
+    return <h1 className={styles.large_title}>Hello, {user ? (user?.first_name + " " + user?.last_name) : <Skeleton width={300} height={45} />}</h1>
   }
 
 }
@@ -25,6 +27,8 @@ const getSession = ({ sessions, department, course_number, session_id }) => {
   // * Verfy the structure of session object
 
   console.log({ sessions, session_id, course_number })
+
+  if (!sessions) return
 
   return sessions.find(x => (x.session_name === session_id) && (x.course_string.toLowerCase() === (department + " " + course_number)));
   // && (x.course_id === course_number)
@@ -45,9 +49,9 @@ function Navbar() {
   // { course_id: "CS 123", session_id: "101", course_name: "Data Structures and Algorithms", term: "Fall 2020", url: "/cs/123/session/101" }
   // ]
 
-  if (isLoading) {
-    return "Loading"
-  }
+  // if (isLoading) {
+  //   return "Loading"
+  // }
 
   return (
     <nav className={styles.nav_container}>

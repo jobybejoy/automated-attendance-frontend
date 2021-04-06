7291ddad52807894fc13da51a0e0dd0df0ddf561
Joby Bejoy <57924079+jobybejoy@users.noreply.github.com>
2021-04-05 20:24:17 -0400

    Session Item Loading state and URL

--- src/components/Session/SessionList/SessionItem.js
+++ src/components/Session/SessionList/SessionItem.js
@@ -1,12 +1,27 @@
 import styles from "./SessionList.module.css"
 import { Link } from 'react-router-dom'
+import Skeleton from 'react-loading-skeleton';
 
-export default function SessionItem({ session, key }) {
+export default function SessionItem({ session }) {
+
+  if (!session) {
+    return <Skeleton
+      style={{ width: "100%", borderRadius: "0.5rem", marginBottom: "0.6rem" }} height={60} />
+  }
+
+  const getSessionURL = (session) => {
+    if (!session) return
+    const [department, course_id] = session.course_string.split(" ")
+    return `/${department}/${course_id}/session/${session.session_name}`
+  }
+
+  const url = getSessionURL(session)
+
   return (
-    <Link to={session.url} key={key}>
+    <Link to={url}>
       <div className={styles.session_card}>
         <div>
-          <div className={styles.course_id}>{session?.course_string}</div>
+          <div className={styles.course_id}>{session?.course_string.toUpperCase()}</div>
           <div className={styles.session_id}>{session?.session_name}</div>
         </div>
         <div className={styles.course_name}>{session?.course_name}</div>

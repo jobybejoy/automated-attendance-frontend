c9a292f62d2750ae4c3f07a72c5804a629e1b7f1
Joby Bejoy <57924079+jobybejoy@users.noreply.github.com>
2021-04-11 22:03:18 -0400

    Remove getSession fn. and Added Error states

--- src/containers/Layout/Navbar/index.js
+++ src/containers/Layout/Navbar/index.js
@@ -10,6 +10,7 @@ import { UserContext } from "../../../context/UserContext"
 import { SessionsContext } from "../../../context/SessionsContext"
 
 import Skeleton from 'react-loading-skeleton';
+import ErrorPage from "../../../pages/_Error"
 
 import { findSession } from "../../../helpers/findSession"
 
@@ -24,22 +25,11 @@ export const Header = ({ department, course_number, session_id, sessions, user }
 
 }
 
-export const getSession = ({ sessions, department, course_number, session_id }) => {
-  // ! currently finding session only with sessionID
-  // * Verfy the structure of session object
 
-  // console.log({ sessions, session_id, course_number })
-
-  if (!sessions) return
-
-  return sessions.find(x => (x.session_name === session_id) && (x.course_string.toLowerCase() === (department + " " + course_number)));
-  // && (x.course_id === course_number)
-  // }
-}
 
 function NavbarContainer() {
-  const { user } = useContext(UserContext)
-  const { sessions } = useContext(SessionsContext)
+  const { user, isError: isUserError } = useContext(UserContext)
+  const { sessions, isError: isSessionsError } = useContext(SessionsContext)
 
   const { department, course_number, session_id } = useParams();
 
@@ -47,22 +37,20 @@ function NavbarContainer() {
   //   return "Loading"
   // }
 
+  if (isUserError) {
+    return <ErrorPage error={isUserError} />
+  }
+
+  if (isSessionsError) {
+    return <ErrorPage error={isSessionsError} />
+  }
+
+
   return <Navbar user={user} sessions={sessions} department={department} course_number={course_number} session_id={session_id} />
 
 }
 
 export function Navbar({ user, sessions, department, course_number, session_id }) {
-
-
-  // Gets the department,course_id and  params from url
-
-
-  // const sessions = [
-  //   { course_id: "CS 123", session_id: "111", course_name: "Data Structures and Algorithms", term: "Fall 2020", url: "/cs/123/session/111" },
-  // { course_id: "CS 656", session_id: "001", course_name: "Machine Learning", term: "Fall 2020", url: "/cs/656/session/001" },
-  // { course_id: "CS 123", session_id: "101", course_name: "Data Structures and Algorithms", term: "Fall 2020", url: "/cs/123/session/101" }
-  // ]
-
 
   return (
     <nav className={styles.nav_container}>

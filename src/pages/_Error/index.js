1155b210a4c7c7d3142098ccc86e9f69e9354ae3
Joby Bejoy <57924079+jobybejoy@users.noreply.github.com>
2021-04-11 22:05:21 -0400

    Added Error Page

--- src/pages/_Error/index.js
+++ src/pages/_Error/index.js
@@ -3,14 +3,15 @@ import styles from "./Error.module.css"
 
 export default function Error({ error }) {
 
-  console.log("In Error Page");
-  console.log({ error })
+  // console.log("In Error Page");
+  // console.log({ error })
 
   // console.log({ error.statusText })
   const obj_string = JSON.stringify(error)
   return (
     <LayoutOnly>
       <div className={styles.ErrorContainer}>
+        <h1 className={styles.error_page_header}>Opps! We have an error</h1>
         <h1 className={styles.error_status}>{error.status}</h1>
         <h2 className={styles.error_heading}>Server Message</h2>
         <p className={styles.error_message}>{error.info.message}</p>
